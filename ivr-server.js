5d7ede156f8a836dc62590f247700e3dd23e8c16
Hongbo Tian <t00117009@Mikes-MacBook-Pro.local>
2014-05-15 11:47:29 -0700

    rename server for easy manage with forever

---
+++ ivr-server.js
@@ -0,0 +1,70 @@
+var express = require('express');
+var path = require('path');
+var http =require('http');
+var favicon = require('static-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var routes = require('./routes/index');
+var users = require('./routes/users');
+var ivr = require('./routes/ivr');
+
+var app = express();
+var server = http.createServer(app); 
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(favicon());
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.use('/', routes);
+app.use('/users', users);
+app.use('/ivr', ivr);
+
+/// catch 404 and forwarding to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+/// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});
+
+
+module.exports = app;
+
+var port = process.env.NODE_PORT || 3000;
+server.listen(port,function(){
+//client.subscribe('my/topic');
+console.log('Express app started on port ' + port);
+}); 
