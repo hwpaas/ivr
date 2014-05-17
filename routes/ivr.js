b736f512ce3c1afe17dc266beaa6b91ca142ca28
Hongbo Tian <t00117009@Mikes-MacBook-Pro.local>
2014-05-16 17:48:33 -0700

    find timezone by phone number

--- routes/ivr.js
+++ routes/ivr.js
@@ -43,7 +43,7 @@ plivoivr.on('welcome/:p_id', function(params, response) {
 			finishOnKey: '234567890#*',
 			timeout: 20
 		}, function() {
-			response.speak("This is from Tele Med IVR. We found irregular data from your monitor. We would like to make an appointment for you. Press 1 to make an appointment with doctors in U S. Press 2 to make an appointment with doctors in China. Otherwise please hang up.")
+			response.speak("This is from Tele Med IVR. We found irregular data from your monitor. We would like to make an appointment for you. Press 1 to make an appointment. Otherwise please hang up.")
 		})
 		.speak("Sorry, I didn't catch that. Please hangup.");
 
@@ -55,6 +55,15 @@ plivoivr.on('welcome/:p_id', function(params, response) {
 .on('appointment/:p_id', function(params, response) {
 	var p_id = params.p_id;
 	var plivo_response = response;
+	var p_number = params.To;
+
+	var timezone="-07:00";
+	if(p_number[0]=="1"){
+		timezone='-07:00';
+	}
+	else if (p_number[0]=="8"){
+		timezone='+08:00';
+	}
 
 	// Look at what digit the user pressed to determine what to do next.
 	switch (params.Digits) {
@@ -63,7 +72,7 @@ plivoivr.on('welcome/:p_id', function(params, response) {
 			
 			request.post(
 				callback_url,
-				{form: {p_id: p_id, timezone: '-07:00'}},
+				{form: {p_id: p_id, timezone: timezone}},
 				function (error, response, body) {
 					if (!error && response.statusCode == 200) {
 						console.log(body);
@@ -101,48 +110,6 @@ plivoivr.on('welcome/:p_id', function(params, response) {
 				}
 			);
 			break;
-		case '2':
-			console.log("Patient " + p_id + " is making a new appointment.");
-			
-			request.post(
-				callback_url,
-				{form: {p_id: p_id, timezone: '+08:00'}},
-				function (error, response, body) {
-					if (!error && response.statusCode == 200) {
-						console.log(body);
-
-						var isCalled = false;
-						
-						var AppStartTime = JSON.parse(body).AppStartTime;
-						var date = moment(AppStartTime);
-						var now = moment();
-						var difference = moment.utc(date.diff(now)).format("HH:mm:ss");
-						var differenceArray = difference.toString().split(":");
-						var hour = parseInt(differenceArray[0]),
-							minute = parseInt(differenceArray[1]);
-
-						plivo_response
-							.speak('Your appointment is booked! It will start at ' + hour + " hours")
-							.wait(1)
-							.speak(minute + " minutes later.")
-							.wait(1)
-							.speak('Thanks for using this service. Goodbye.');
-						if(!isCalled){
-							plivo_response.send();
-							isCalled = true;
-						}
-					} else {
-						console.log("error is " + error);
-						if (response.statusCode) {
-							console.log("response is " + response.statusCode);
-						}
-						console.log("body is " + body);
-						plivo_response
-							.speak("Sorry, your doctor is not available today. Goodbye.")
-							.send();
-					}
-				}
-			);
 		break;					
 	}
 
