6babd3f277ecd6f78b3610bc9b5e50806375bfff
Ming He <hmchem@gmail.com>
2014-07-21 16:16:08 -0700

    this is just ivr client

--- routes/ivr.js
+++ routes/ivr.js
@@ -12,7 +12,10 @@ var plivoivr = new plivode.App({
 		rootUrl: config.plivoivr.rootUrl
 	});
 
-/* Handles POST from other applications */
+/* Handles POST from other applications 
+The actual logic is on ivr server, which we do not care here.
+After initiate the call, it is done.
+*/
 router.post('/', function(req, res) {
 	//get patient info from the patientApiUrl
 	request.get(config.patientApiUrl,function(err, response, body){
@@ -20,93 +23,13 @@ router.post('/', function(req, res) {
 			console.log('err get patient info from '+config.patientApiUrl);
 			res.json(500);
 		}
-		var patientInfo = body;
-		console.log(patientInfo);
+		var patientInfo = JSON.parse(body);
 		var p_number = patientInfo.phoneNumber,
 			p_id = patientInfo.patientId,
 			call_from = config.callerNum;
 	  res.json({ message: 'patient called', pid: p_id });
 		plivoivr.Call.outbound(call_from, p_number, ['welcome', p_id]);
 	});
-
 });
-
-plivoivr
-	.on('welcome/:p_id', function(params, response) {
-		var p_id = params.p_id;
-		console.log('calling patient ' + p_id);
-
-		response.speak("Hi.") 
-			.getDigits({
-				action: ['appointment', p_id],
-				numDigits: 1,
-				validDigits: '1',
-				finishOnKey: '234567890#*',
-				timeout: 20
-			}, function() {
-				response.speak("This is from Tele Med IVR. We found irregular data from your monitor. We would like to make an appointment for you. Press 1 to make an appointment. Otherwise please hang up.")
-			})
-			.speak("Sorry, I didn't catch that. Please hangup.");
-	})
-
-	.on('appointment/:p_id', function(params, response) {
-		var p_id = params.p_id;
-		var plivo_response = response;
-		var p_number = params.To;
-
-		var timezone="-07:00";
-		if(p_number[1]=="1"){
-			timezone='-07:00';
-		}
-		else if (p_number[1]=="8"){
-			timezone='+08:00';
-		}
-
-		// Look at what digit the user pressed to determine what to do next.
-		switch (params.Digits) {
-			case '1':
-				console.log("Patient " + p_id + " is making a new appointment.");
-				
-				request.post(config.callback_url,{form: {p_id: p_id, timezone: timezone}},
-					function (error, response, body) {
-						if (!error && response.statusCode == 200) {
-							console.log(body);
-														
-							var AppStartTime = JSON.parse(body).AppStartTime;
-							var date = moment(AppStartTime);
-							var now = moment();
-							var difference = moment.utc(date.diff(now)).format("HH:mm:ss");
-							var differenceArray = difference.toString().split(":");
-							var hour = parseInt(differenceArray[0]),
-								minute = parseInt(differenceArray[1]);
-
-							plivo_response
-								.speak('Your appointment is booked! It will start at ' + hour + " hours")
-								.wait(1)
-								.speak(minute + " minutes later.")
-								.wait(1)
-								.speak('Thanks for using this service. Goodbye.');
-
-						} else {
-							console.log("error is " + error);
-							if (response.statusCode) {
-								console.log("response is " + response.statusCode);
-							}
-							console.log("body is " + body);
-							plivo_response
-								.speak("Sorry, your doctor is not available today. Goodbye.")
-								.send();
-						}
-					}
-				);
-				break;
-			break;					
-		}
-
-	})
-	.on('hangup', function(params) {
-		console.log('call ended with: ' + params.From);
-		console.log(params);
-	});
 
 module.exports = router;
